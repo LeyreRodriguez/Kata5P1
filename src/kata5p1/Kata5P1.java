c599289a68b16378be23bde201116653d488d441
Leyre <Leyre@DESKTOP-NO4GJ0P>
2021-11-16 17:14:45 +0000

    Kata5P1 version3

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,24 +1,25 @@
 package kata5p1;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
-
+import java.util.List;
 public class Kata5P1 {
+
     public static void main(String[] args) {
-        createNewTable();
-    }
-    public static void createNewTable() {
+        String fichero = "email.txt";
+        List<String> lista = new MailListReader().read(fichero);
+
         String url = "jdbc:sqlite:Kata5.db";
-        String sql = "CREATE TABLE IF NOT EXISTS direcc_email ( \n id integer PRIMARY KEY AUTOINCREMENT, \n direccion text NOT NULL);";
-        try(Connection conn = DriverManager.getConnection(url);
-                Statement stmt = conn.createStatement()) {
-            stmt.execute(sql);
-            System.out.println("Tabla creada");
-        } catch (SQLException e) {
+        String sql= "INSERT INTO EMAIL(Mail) VALUES(?)";
+        try (Connection conn = DriverManager.getConnection(url) ;
+            PreparedStatement pstmt= conn.prepareStatement(sql)) {
+            for (String list: lista) {
+                pstmt.setString(1, list);
+                pstmt.executeUpdate();
+            }
+        } catch(SQLException e) {
             System.out.println(e.getMessage());
         }
     }
-    
 }
