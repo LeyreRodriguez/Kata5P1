2c24e1ed80ff74ebd826a8135a3f73a3cd0b7e7a
Leyre <Leyre@DESKTOP-NO4GJ0P>
2021-11-16 17:13:11 +0000

    Kata5P1 version2

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,47 +1,26 @@
 package kata5p1;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
 public class Kata5P1 {
 
 
     public static void main(String[] args) {
-        selectAll();
+        createNewTable();
     }
-
-    private static Connection connect() {
+    public static void createNewTable() {
         String url = "jdbc:sqlite:Kata5.db";
-        Connection conn = null;
-        try {
-
-            conn = DriverManager.getConnection(url);
+        String sql = "CREATE TABLE IF NOT EXISTS direcc_email ( \n id integer PRIMARY KEY AUTOINCREMENT, \n direccion text NOT NULL);";
+        try(Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println("Tabla creada");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-        return conn;
     }
-
-    public static void selectAll() {
-        String sql = "SELECT * FROM PEOPLE";
-
-        try (Connection conn = Kata5P1.connect();
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery(sql)) {
-            while (rs.next()) {
-                System.out.println(rs.getInt("id") + "\t" +
-                        rs.getString("Name") + "\t" +
-                        rs.getString("Apellidos") + "\t" +
-                        rs.getString("Departamento") + "\t");
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-
-        }
-    }
-
-
-
+    
 }
